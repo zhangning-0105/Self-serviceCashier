5853b315fd85826ab2764f5a0792d5a81ea1b48f
张宁 <380781697@qq.com>
2020-08-06 10:49:46 +0800

    1.修改部分内容

--- Self-serviceCashier/mainwindow.Designer.cs
+++ Self-serviceCashier/mainwindow.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainwindow));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.xButton1 = new Self_serviceCashier.XButton();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label14 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
@@ -53,6 +54,7 @@
             this.label16 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
+            this.xButton2 = new Self_serviceCashier.XButton();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -66,8 +68,6 @@
             this.数量 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.xButton1 = new Self_serviceCashier.XButton();
-            this.xButton2 = new Self_serviceCashier.XButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel2.SuspendLayout();
@@ -108,6 +108,30 @@
             this.pictureBox3.TabIndex = 23;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // xButton1
+            // 
+            this.xButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.xButton1.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton1.EnterForeColor = System.Drawing.Color.White;
+            this.xButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.xButton1.FlatAppearance.BorderSize = 0;
+            this.xButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.xButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.xButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.xButton1.Font = new System.Drawing.Font("黑体", 18F);
+            this.xButton1.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton1.HoverForeColor = System.Drawing.Color.White;
+            this.xButton1.Location = new System.Drawing.Point(1281, 60);
+            this.xButton1.Name = "xButton1";
+            this.xButton1.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton1.PressForeColor = System.Drawing.Color.White;
+            this.xButton1.Radius = 18;
+            this.xButton1.Size = new System.Drawing.Size(198, 68);
+            this.xButton1.TabIndex = 22;
+            this.xButton1.Text = "  清空";
+            this.xButton1.UseVisualStyleBackColor = true;
+            this.xButton1.Click += new System.EventHandler(this.xButton1_Click);
             // 
             // panel2
             // 
@@ -356,6 +380,31 @@
             this.label15.TabIndex = 30;
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // xButton2
+            // 
+            this.xButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.xButton2.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton2.EnterForeColor = System.Drawing.Color.White;
+            this.xButton2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.xButton2.FlatAppearance.BorderSize = 0;
+            this.xButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.xButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.xButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.xButton2.Font = new System.Drawing.Font("黑体", 18F);
+            this.xButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton2.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton2.HoverForeColor = System.Drawing.Color.White;
+            this.xButton2.Location = new System.Drawing.Point(567, 486);
+            this.xButton2.Name = "xButton2";
+            this.xButton2.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.xButton2.PressForeColor = System.Drawing.Color.White;
+            this.xButton2.Radius = 50;
+            this.xButton2.Size = new System.Drawing.Size(219, 61);
+            this.xButton2.TabIndex = 24;
+            this.xButton2.Text = "立即关闭";
+            this.xButton2.UseVisualStyleBackColor = true;
+            this.xButton2.Click += new System.EventHandler(this.xButton2_Click);
+            // 
             // pictureBox4
             // 
             this.pictureBox4.ErrorImage = null;
@@ -370,7 +419,7 @@
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("黑体", 18F);
-            this.label5.Location = new System.Drawing.Point(310, 387);
+            this.label5.Location = new System.Drawing.Point(310, 406);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(331, 48);
             this.label5.TabIndex = 17;
@@ -416,14 +465,14 @@
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(1074, 674);
@@ -477,55 +526,6 @@
             this.数量.Name = "数量";
             this.数量.ReadOnly = true;
             this.数量.Width = 54;
-            // 
-            // xButton1
-            // 
-            this.xButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.xButton1.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton1.EnterForeColor = System.Drawing.Color.White;
-            this.xButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.xButton1.FlatAppearance.BorderSize = 0;
-            this.xButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.xButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.xButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.xButton1.Font = new System.Drawing.Font("黑体", 18F);
-            this.xButton1.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton1.HoverForeColor = System.Drawing.Color.White;
-            this.xButton1.Location = new System.Drawing.Point(1281, 60);
-            this.xButton1.Name = "xButton1";
-            this.xButton1.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton1.PressForeColor = System.Drawing.Color.White;
-            this.xButton1.Radius = 18;
-            this.xButton1.Size = new System.Drawing.Size(198, 68);
-            this.xButton1.TabIndex = 22;
-            this.xButton1.Text = "  清空";
-            this.xButton1.UseVisualStyleBackColor = true;
-            this.xButton1.Click += new System.EventHandler(this.xButton1_Click);
-            // 
-            // xButton2
-            // 
-            this.xButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.xButton2.EnterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton2.EnterForeColor = System.Drawing.Color.White;
-            this.xButton2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.xButton2.FlatAppearance.BorderSize = 0;
-            this.xButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.xButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.xButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.xButton2.Font = new System.Drawing.Font("黑体", 18F);
-            this.xButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton2.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton2.HoverForeColor = System.Drawing.Color.White;
-            this.xButton2.Location = new System.Drawing.Point(567, 486);
-            this.xButton2.Name = "xButton2";
-            this.xButton2.PressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
-            this.xButton2.PressForeColor = System.Drawing.Color.White;
-            this.xButton2.Radius = 50;
-            this.xButton2.Size = new System.Drawing.Size(219, 61);
-            this.xButton2.TabIndex = 24;
-            this.xButton2.Text = "立即关闭";
-            this.xButton2.UseVisualStyleBackColor = true;
-            this.xButton2.Click += new System.EventHandler(this.xButton2_Click);
             // 
             // mainwindow
             // 
